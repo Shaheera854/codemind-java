f6c830733154015f50346495449490395c3f8b69
Shaheera854 <96729999+Shaheera854@users.noreply.github.com>
2022-08-26 12:07:58 +0530

    import java.util.Scanner; class Triangle {   public static void main(String args[])   {     Scanner sc=new Scanner(System.in);     float area,r;     r=sc.nextInt();     area=(float)(r*r*3.14);     System.out.format("%.2f",area);   } }

---
+++ Area_of_a_circle.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class Triangle
+{
+  public static void main(String args[])
+  {
+    Scanner sc=new Scanner(System.in);
+    float area,r;
+    r=sc.nextInt();
+    area=(float)(r*r*3.14);
+    System.out.format("%.2f",area);
+  }
+}
