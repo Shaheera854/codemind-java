8a13d0f13b894f1b286ff568ce47a6ac5a43c51c
Shaheera854 <96729999+Shaheera854@users.noreply.github.com>
2022-08-30 19:58:55 +0530

    import java.util.Scanner; class min {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,a,b,i,j,min=0;         n=sc.nextInt();         int[] arr=new int[n];         for(i=0;i<n;i++)         {             arr[i]=sc.nextInt();         }         a=sc.nextInt();         b=sc.nextInt();         for(i=a;i<=b;i++)         {             for(j=0;j<n;j++)             {                 if(i==arr[j] && (min==0 || min>i))                 {                     min=arr[j];                     break;                 }             }         }         if(min==0)         {             System.out.println("-1");         }         else         {             System.out.println(min);         }     } }

---
+++ minimum_between_A_and_B.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+class min
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,a,b,i,j,min=0;
+        n=sc.nextInt();
+        int[] arr=new int[n];
+        for(i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        a=sc.nextInt();
+        b=sc.nextInt();
+        for(i=a;i<=b;i++)
+        {
+            for(j=0;j<n;j++)
+            {
+                if(i==arr[j] && (min==0 || min>i))
+                {
+                    min=arr[j];
+                    break;
+                }
+            }
+        }
+        if(min==0)
+        {
+            System.out.println("-1");
+        }
+        else
+        {
+            System.out.println(min);
+        }
+    }
+}
