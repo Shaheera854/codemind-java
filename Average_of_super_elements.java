ee229d6249a89f729cd45d08b26ac82d62f3fba8
Shaheera854 <96729999+Shaheera854@users.noreply.github.com>
2022-08-30 21:03:39 +0530

    import java.util.Scanner; class kth {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,i,s=0,j,c,k=0;         float avg;         n=sc.nextInt();         int[] arr=new int[n];         for(i=0;i<n;i++)         {             arr[i]=sc.nextInt();         }         for(i=0;i<n;i++)         {             c=0;             for(j=0;j<n;j++)             {                 if(arr[i]==arr[j] && i>j)                 {                     break;                 }                 if(arr[i]==arr[j])                 {                     c++;                 }             }             if(c==arr[i])             {                 s+=arr[i];                 k++;             }         }         if(k==0)         {             System.out.println("-1");         }         else         {             avg=(float)s/k;             System.out.format("%.2f",avg);         }     } }

---
+++ Average_of_super_elements.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+class kth
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,i,s=0,j,c,k=0;
+        float avg;
+        n=sc.nextInt();
+        int[] arr=new int[n];
+        for(i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        for(i=0;i<n;i++)
+        {
+            c=0;
+            for(j=0;j<n;j++)
+            {
+                if(arr[i]==arr[j] && i>j)
+                {
+                    break;
+                }
+                if(arr[i]==arr[j])
+                {
+                    c++;
+                }
+            }
+            if(c==arr[i])
+            {
+                s+=arr[i];
+                k++;
+            }
+        }
+        if(k==0)
+        {
+            System.out.println("-1");
+        }
+        else
+        {
+            avg=(float)s/k;
+            System.out.format("%.2f",avg);
+        }
+    }
+}
