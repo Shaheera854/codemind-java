136e7e2dc230a7299d5ed03aeb63bbe17f247136
Shaheera854 <96729999+Shaheera854@users.noreply.github.com>
2022-08-30 21:10:57 +0530

    import java.util.Scanner; class kth {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,i,s=0,j,c,k=0,min=0,max=0;         float avg;         n=sc.nextInt();         int[] arr=new int[n];         for(i=0;i<n;i++)         {             arr[i]=sc.nextInt();         }         for(i=0;i<n;i++)         {             c=0;             for(j=0;j<n;j++)             {                 if(arr[i]==arr[j] && i>j)                 {                     break;                 }                 if(arr[i]==arr[j])                 {                     c++;                 }             }             if(c==arr[i])             {                 if(min==0 || min>arr[i])                 {                     min=arr[i];                 }                 if(max<arr[i])                 {                     max=arr[i];                 }                 k++;             }         }         if(k==0)         {             System.out.println("-1");         }         else         {             System.out.print(min+" "+max);         }     } }

---
+++ Maximum_and_Minimum.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+class kth
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,i,s=0,j,c,k=0,min=0,max=0;
+        float avg;
+        n=sc.nextInt();
+        int[] arr=new int[n];
+        for(i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        for(i=0;i<n;i++)
+        {
+            c=0;
+            for(j=0;j<n;j++)
+            {
+                if(arr[i]==arr[j] && i>j)
+                {
+                    break;
+                }
+                if(arr[i]==arr[j])
+                {
+                    c++;
+                }
+            }
+            if(c==arr[i])
+            {
+                if(min==0 || min>arr[i])
+                {
+                    min=arr[i];
+                }
+                if(max<arr[i])
+                {
+                    max=arr[i];
+                }
+                k++;
+            }
+        }
+        if(k==0)
+        {
+            System.out.println("-1");
+        }
+        else
+        {
+            System.out.print(min+" "+max);
+        }
+    }
+}
