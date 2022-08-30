ee7307548fd00332495168d3e60910ca0cc1bbd5
Shaheera854 <96729999+Shaheera854@users.noreply.github.com>
2022-08-30 21:46:56 +0530

    import java.util.Scanner; class kth {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,i,s=0,j,c,a,b,t;         float avg;         n=sc.nextInt();         int[] arr=new int[n];         for(i=0;i<n;i++)         {             arr[i]=sc.nextInt();         }         a=sc.nextInt();         b=sc.nextInt();         for(i=0;i<n;i++)         {             t=0;             for(j=a;j<=b;j++)             {                 if(arr[i]==j)                 {                     t=1;                     break;                 }             }             if(t==0)             {                 s+=arr[i];             }         }         System.out.print(s);     } }

---
+++ sum_of_elements_not_between_A_and_B.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+class kth
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,i,s=0,j,c,a,b,t;
+        float avg;
+        n=sc.nextInt();
+        int[] arr=new int[n];
+        for(i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        a=sc.nextInt();
+        b=sc.nextInt();
+        for(i=0;i<n;i++)
+        {
+            t=0;
+            for(j=a;j<=b;j++)
+            {
+                if(arr[i]==j)
+                {
+                    t=1;
+                    break;
+                }
+            }
+            if(t==0)
+            {
+                s+=arr[i];
+            }
+        }
+        System.out.print(s);
+    }
+}
