fee345c0991e261614803fbf8d3e9c2231b0d778
Shaheera854 <96729999+Shaheera854@users.noreply.github.com>
2022-08-31 15:22:42 +0530

    import java.util.Scanner; class primes {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,i,s=0,j,c=0,t,min,max,ind_min,ind_max,x,y;         float avg;         n=sc.nextInt();         int[] a=new int[n];         for(i=0;i<n;i++)         {             a[i]=sc.nextInt();         }         min=a[0];         ind_min=0;         max=a[0];         ind_max=0;         for(i=1;i<n;i++)         {             if(min>a[i])             {                 min=a[i];                 ind_min=i;             }             if(max<a[i])             {                 max=a[i];                 ind_max=i;             }         }         if(ind_min<ind_max)         {             x=ind_min;             y=ind_max;         }         else         {             x=ind_max;             y=ind_min;         }         for(i=x;i<y+1;i++)         {             if(a[i]==1)             {                 continue;             }             else             {                 t=0;                 for(j=2;j<(a[i]/2)+1;j++)                 {                     if(a[i]%j==0)                     {                         t=1;                         break;                     }                 }                 if(t==0)                 {                    c++;                 }             }         }         System.out.print(c);     }
    }

---
+++ Primes_between_min_and_max.java
@@ -0,0 +1,68 @@
+import java.util.Scanner;
+class primes
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,i,s=0,j,c=0,t,min,max,ind_min,ind_max,x,y;
+        float avg;
+        n=sc.nextInt();
+        int[] a=new int[n];
+        for(i=0;i<n;i++)
+        {
+            a[i]=sc.nextInt();
+        }
+        min=a[0];
+        ind_min=0;
+        max=a[0];
+        ind_max=0;
+        for(i=1;i<n;i++)
+        {
+            if(min>a[i])
+            {
+                min=a[i];
+                ind_min=i;
+            }
+            if(max<a[i])
+            {
+                max=a[i];
+                ind_max=i;
+            }
+        }
+        if(ind_min<ind_max)
+        {
+            x=ind_min;
+            y=ind_max;
+        }
+        else
+        {
+            x=ind_max;
+            y=ind_min;
+        }
+        for(i=x;i<y+1;i++)
+        {
+            if(a[i]==1)
+            {
+                continue;
+            }
+            else
+            {
+                t=0;
+                for(j=2;j<(a[i]/2)+1;j++)
+                {
+                    if(a[i]%j==0)
+                    {
+                        t=1;
+                        break;
+                    }
+                }
+                if(t==0)
+                {
+                   c++; 
+                }
+            }
+        }
+        System.out.print(c);
+    }
+    
+}
