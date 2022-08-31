96865fffcf5a68ee8fc8ffb025fcff39acaa8288
Shaheera854 <96729999+Shaheera854@users.noreply.github.com>
2022-08-31 16:08:07 +0530

    import java.util.Scanner; class count {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,c=0,i;         n=sc.nextInt();         int[] a=new int[n];         for(i=0;i<n;i++)         {             a[i]=sc.nextInt();         }         for(i=0;i<n-2;i++)         {             if(a[i+1]%2!=0 && a[i]%2!=0 && a[i+2]%2!=0)             {                 c++;             }         }         System.out.print(c);     } }

---
+++ count_the_add_numbers_inbetween_odd_numbers.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+class count
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,c=0,i;
+        n=sc.nextInt();
+        int[] a=new int[n];
+        for(i=0;i<n;i++)
+        {
+            a[i]=sc.nextInt();
+        }
+        for(i=0;i<n-2;i++)
+        {
+            if(a[i+1]%2!=0 && a[i]%2!=0 && a[i+2]%2!=0)
+            {
+                c++;
+            }
+        }
+        System.out.print(c);
+    }
+}
