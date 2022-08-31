795cb3eb4b259f94f4bff36269a0ebe5a2a1e7f2
Shaheera854 <96729999+Shaheera854@users.noreply.github.com>
2022-08-31 15:16:04 +0530

    import java.util.Scanner; class primes {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,i,s=0,j,c=0,t,k;         float avg;         n=sc.nextInt();         int[] a=new int[n];         for(i=0;i<n;i++)         {             a[i]=sc.nextInt();         }         k=sc.nextInt();         for(i=0;i<n;i++)         {             if(a[i]==1)             {                 continue;             }             else             {                 t=0;                 for(j=2;j<(a[i]/2)+1;j++)                 {                     if(a[i]%j==0)                     {                         t=1;                         break;                     }                 }                 if(t==0 && a[i]<=k)                 {                    c++;                 }             }         }         System.out.print(c);     }
    }

---
+++ primes_less_than_k.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+class primes
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,i,s=0,j,c=0,t,k;
+        float avg;
+        n=sc.nextInt();
+        int[] a=new int[n];
+        for(i=0;i<n;i++)
+        {
+            a[i]=sc.nextInt();
+        }
+        k=sc.nextInt();
+        for(i=0;i<n;i++)
+        {
+            if(a[i]==1)
+            {
+                continue;
+            }
+            else
+            {
+                t=0;
+                for(j=2;j<(a[i]/2)+1;j++)
+                {
+                    if(a[i]%j==0)
+                    {
+                        t=1;
+                        break;
+                    }
+                }
+                if(t==0 && a[i]<=k)
+                {
+                   c++; 
+                }
+            }
+        }
+        System.out.print(c);
+    }
+    
+}
