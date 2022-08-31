0448907b972aafe241a5b4c00e47e862c0dba1f0
Shaheera854 <96729999+Shaheera854@users.noreply.github.com>
2022-08-31 15:55:45 +0530

    import java.util.Scanner; class avg {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,i,s=0,c=0,avg;         n=sc.nextInt();         int[] a=new int[n];         for(i=0;i<n;i++)         {             a[i]=sc.nextInt();             s+=a[i];         }         avg=s/n;         for(i=0;i<n;i++)         {             if(a[i]<=avg)             {                 c++;             }         }         System.out.print(c);     } }

---
+++ elements_smaller_than_average.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+class avg
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,i,s=0,c=0,avg;
+        n=sc.nextInt();
+        int[] a=new int[n];
+        for(i=0;i<n;i++)
+        {
+            a[i]=sc.nextInt();
+            s+=a[i];
+        }
+        avg=s/n;
+        for(i=0;i<n;i++)
+        {
+            if(a[i]<=avg)
+            {
+                c++;
+            }
+        }
+        System.out.print(c);
+    }
+}
