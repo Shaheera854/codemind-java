34e82f768d02a7fe0dfb6a255a677f4c848a1606
Shaheera854 <96729999+Shaheera854@users.noreply.github.com>
2022-08-31 23:16:20 +0530

    import java.util.Scanner; class count {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,i,ele,c=0;         n=sc.nextInt();         int[] a=new int[n];         for(i=0;i<n;i++)         {             a[i]=sc.nextInt();         }         ele=sc.nextInt();         for(i=0;i<n;i++)         {             if(a[i]==ele)             {                 c++;                 System.out.print("True");                 break;             }         }         if(c==0)         {             System.out.print("False");         }     } }

---
+++ element_search_in_an_array.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+class count
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,i,ele,c=0;
+        n=sc.nextInt();
+        int[] a=new int[n];
+        for(i=0;i<n;i++)
+        {
+            a[i]=sc.nextInt();
+        }
+        ele=sc.nextInt();
+        for(i=0;i<n;i++)
+        {
+            if(a[i]==ele)
+            {
+                c++;
+                System.out.print("True");
+                break;
+            }
+        }
+        if(c==0)
+        {
+            System.out.print("False");
+        }
+    }
+}
