af370debd7dca826fe0821decf84598506737839
Shaheera854 <96729999+Shaheera854@users.noreply.github.com>
2022-09-30 19:04:09 +0530

    import java.util.Scanner; class Weight {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,t,s=0;         n=sc.nextInt();         int arr[]=new int[n];         for(int i=0;i<n;i++)         {             arr[i]=sc.nextInt();         }         t=sc.nextInt();         for(int i=0;i<n;i++)         {             if(arr[i]<=t)             {                 s+=1;             }             else             {                 s+=2;             }         }         System.out.println(s);     } }

---
+++ Airport__authority.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+class Weight
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,t,s=0;
+        n=sc.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        t=sc.nextInt();
+        for(int i=0;i<n;i++)
+        {
+            if(arr[i]<=t)
+            {
+                s+=1;
+            }
+            else
+            {
+                s+=2;
+            }
+        }
+        System.out.println(s);
+    }
+}
