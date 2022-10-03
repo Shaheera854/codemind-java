39f65337bdba831b07f55343de91e1c63d3138c4
Shaheera854 <96729999+Shaheera854@users.noreply.github.com>
2022-10-03 17:24:40 +0530

    import java.util.Scanner; class Homeless {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int people[]=new int[n];         int house[]=new int[n];         for(int i=0;i<n;i++)         {             people[i]=sc.nextInt();         }         for(int i=0;i<n;i++)         {             house[i]=sc.nextInt();         }         int count=0;         for(int i=0;i<n;i++)         {             int c=1;             for(int j=0;j<n;j++)             {                 if(people[i]<=house[j])                 {                     c=0;                     house[j]=0;                     break;                 }             }             if(c!=0)             count++;         }         System.out.println(count);     } }

---
+++ Find_the_homeless .java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+class Homeless
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int people[]=new int[n];
+        int house[]=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            people[i]=sc.nextInt();
+        }
+        for(int i=0;i<n;i++)
+        {
+            house[i]=sc.nextInt();
+        }
+        int count=0;
+        for(int i=0;i<n;i++)
+        {
+            int c=1;
+            for(int j=0;j<n;j++)
+            {
+                if(people[i]<=house[j])
+                {
+                    c=0;
+                    house[j]=0;
+                    break;
+                }
+            }
+            if(c!=0)
+            count++;
+        }
+        System.out.println(count);
+    }
+}
