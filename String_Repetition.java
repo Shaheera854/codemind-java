33e387ca66d05487577d374f4f7b397ede4d22a7
Shaheera854 <96729999+Shaheera854@users.noreply.github.com>
2022-10-03 17:19:34 +0530

    import java.util.Scanner; class Repetition {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         String s=sc.next();         long n=sc.nextLong();         long len=s.length();         long a=0;         for(int i=0;i<len;i++)         {             if(s.charAt(i)=='a')             a++;         }         long l=n/len;         a*=l;         for(int i=0;i<n%len;i++)         {             if(s.charAt(i)=='a')             a++;         }         System.out.println(a);     } }

---
+++ String_Repetition.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+class Repetition
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String s=sc.next();
+        long n=sc.nextLong();
+        long len=s.length();
+        long a=0;
+        for(int i=0;i<len;i++)
+        {
+            if(s.charAt(i)=='a')
+            a++;
+        }
+        long l=n/len;
+        a*=l;
+        for(int i=0;i<n%len;i++)
+        {
+            if(s.charAt(i)=='a')
+            a++;
+        }
+        System.out.println(a);
+    }
+}
