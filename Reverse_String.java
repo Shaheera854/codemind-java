8973287863bc9c543035ffacfee022c180f24fb3
Shaheera854 <96729999+Shaheera854@users.noreply.github.com>
2022-10-04 16:57:01 +0530

    import java.util.Scanner; class Strings {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         String s=sc.nextLine();         for(int i=s.length()-1;i>=0;i--)         {             System.out.print(s.charAt(i));         }     } }

--- Reverse_String.java
+++ Reverse_String.java
@@ -1,16 +1,13 @@
 import java.util.Scanner;
-
-class reversewordsinstring {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner sc=new Scanner(System.in);
-		String s1=sc.nextLine();
-		String words[]=s1.split(" ");
-		for(int i=words.length-1;i>=0;i--)
-		{
-			System.out.print(words[i]+" ");
-		}
-	}
-
+class Strings
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine();
+        for(int i=s.length()-1;i>=0;i--)
+        {
+            System.out.print(s.charAt(i));
+        }
+    }
 }
